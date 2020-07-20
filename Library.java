85eac4b702bc6c93b21a71e52e0d935aa49c929b
Nastushka_zzz <66745124+anastasiamasalova2020@users.noreply.github.com>
2020-07-20 22:16:10 +0300

    Update Library.java

--- Library.java
+++ Library.java
@@ -6,13 +6,3 @@ public class Library {
         System.out.println("Your final price is "+result);
     }
 }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the initial price of entrance fee to library: ");
-        double ip=sc.nextDouble();
-        System.out.println("Enter the governmental tax(coefficient) : ");
-        double cf=sc.nextDouble();
-        sc.close();
-        price(ip,cf);
-    }
-}
